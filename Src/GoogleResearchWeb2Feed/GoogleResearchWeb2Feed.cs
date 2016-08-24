d5290a93d41b1a852fa3384550c5e3930f470aad
arichika taniguchi <email@arichika.net>
2016-08-25 02:17:56 +0900

    cached / fix link

--- Src/GoogleResearchWeb2Feed/GoogleResearchWeb2Feed.cs
+++ Src/GoogleResearchWeb2Feed/GoogleResearchWeb2Feed.cs
@@ -22,27 +22,16 @@ namespace GoogleResearchWeb2Feed
         private static readonly char[] TrimChars = new[] { '"', ' ', '\r', '\n' };
 
         private static readonly int MaxItemsPerArea = 10;
-        private static readonly int MaxItemsPerFeed = 50;
+        private static readonly int MaxItemsPerFeed = 160;
 
-        private static TimeSpan _cacheTimeSpan;
-
-        private static  SyndicationFeed _resultLastSyndicationFeed;
-        private static DateTime _resultLastDateTimeUtc = DateTime.MinValue;
-
-        public GoogleResearchWeb2Feed(TimeSpan? cacheTimeSpan = null)
+        public GoogleResearchWeb2Feed()
         {
-            _cacheTimeSpan = cacheTimeSpan ?? TimeSpan.FromHours(1);
         }
 
         public async Task<SyndicationFeed> GetFeed(bool useCache = true)
         {
-            if (useCache != false && _resultLastSyndicationFeed != null && _resultLastDateTimeUtc + _cacheTimeSpan >= DateTime.UtcNow)
-                return _resultLastSyndicationFeed;
-
             var pubs = await GetPubs();
-            _resultLastSyndicationFeed = await MakeFeed(pubs.Select(p => p.Value), GrFqdn);  // 2回とっても困りはしないので雑にキャッシュ
-            _resultLastDateTimeUtc = DateTime.UtcNow;
-            return _resultLastSyndicationFeed;
+            return await MakeFeed(pubs.Select(p => p.Value), GrFqdn);
         }
 
         private static async Task<ConcurrentDictionary<string, Article>> GetPubs()
@@ -58,10 +47,16 @@ namespace GoogleResearchWeb2Feed
             {
                 var areaDoc = website.Load(area.OriginalString);
                 var nodes = areaDoc.DocumentNode.SelectNodes("//ul[contains(@class,'pub-list')]/li");
+
+                DateTimeOffset? latestUpdatedDateTime = DateTimeOffset.UtcNow + TimeSpan.FromDays(-1);
+
                 foreach (var item in nodes.Select((node, i) => new { node, i }))
                 {
-                    var a = await GetArticles(item.node);
+                    var a = await GetArticles(item.node, latestUpdatedDateTime);
                     pubs.TryAdd(a.Id, a); // if the article duplicate, to skip.
+
+                    if(a.LastUpdateDateTime != null)
+                        latestUpdatedDateTime = a.LastUpdateDateTime;
 
                     if (item.i == MaxItemsPerArea)
                         break;
@@ -71,18 +66,18 @@ namespace GoogleResearchWeb2Feed
             return pubs;
         }
 
-        private static async Task<Article> GetArticles(HtmlNode node)
+        private static async Task<Article> GetArticles(HtmlNode node, DateTimeOffset? alternateLatestUpdatedDateTime)
         {
             // has search icon ?
             var searchUrl = node.SelectNodes("./a[contains(@class,'search-icon')]")?.FirstOrDefault()?.Attributes["href"]?.Value;
 
             // has pdf icon ?
             var pdfUrl = node.SelectNodes("./a[contains(@class,'pdf-icon')]")?.FirstOrDefault()?.Attributes["href"]?.Value;
-            pdfUrl = pdfUrl?.IndexOf(@"http://", StringComparison.OrdinalIgnoreCase) < 0 ? GrFqdn + pdfUrl : pdfUrl;
+            pdfUrl = pdfUrl?.IndexOf(@"http", StringComparison.OrdinalIgnoreCase) < 0 ? GrFqdn + pdfUrl : pdfUrl;
 
             // has abstract icon ? (== abstract url)
             var abstractUrl = node.SelectNodes("./a[contains(@class,'abstract-icon')]")?.FirstOrDefault()?.Attributes["href"]?.Value;
-            abstractUrl = abstractUrl?.IndexOf(@"http://", StringComparison.OrdinalIgnoreCase) < 0 ? GrFqdn + abstractUrl : abstractUrl;
+            abstractUrl = abstractUrl?.IndexOf(@"http", StringComparison.OrdinalIgnoreCase) < 0 ? GrFqdn + abstractUrl : abstractUrl;
 
             // get title
             var title = node.SelectNodes("./p[@class='pub-title']").FirstOrDefault()?.InnerText.Trim(TrimChars);
@@ -100,17 +95,24 @@ namespace GoogleResearchWeb2Feed
             {
                 Id = id,
                 Title = title,
-                Uri = (abstractUrl == null) ? null : new Uri(abstractUrl),
-                SearchUri = (searchUrl == null) ? null : new Uri(searchUrl),
-                PdfUri = (pdfUrl == null) ? null : new Uri(pdfUrl),
+                Uri = abstractUrl != null
+                    ? new Uri(abstractUrl)
+                    : pdfUrl != null
+                        ? new Uri(pdfUrl)
+                        : searchUrl != null
+                            ? new Uri(searchUrl)
+                            : null,
+                AbstractUri = abstractUrl != null ? new Uri(abstractUrl) : null,
+                PdfUri = pdfUrl != null ? new Uri(pdfUrl) : null,
+                SearchUri = searchUrl != null ? new Uri(searchUrl) : null,
                 Appendix = appendix,
                 LastUpdateDateTime = null,
             };
 
-            if (article.Uri != null)
+            if (article.AbstractUri != null)
                 await AddAbstract(article);
             else
-                article.LastUpdateDateTime = DateTimeOffset.UtcNow;
+                article.LastUpdateDateTime = alternateLatestUpdatedDateTime;
 
             return article;
         }
@@ -134,10 +136,10 @@ namespace GoogleResearchWeb2Feed
             {
                 Id = feedId,
                 Title = new TextSyndicationContent("Google Research"),
-                Description = new TextSyndicationContent("GoogleResearchWeb2Feed"),
+                Description = new TextSyndicationContent("Google Research Web2Feed"),
                 Copyright = new TextSyndicationContent("PUBLIC DOMAIN"),
-                LastUpdatedTime = new DateTimeOffset(DateTime.Now),
-                Generator = "GoogleResearchWeb2Feed",
+                LastUpdatedTime = new DateTimeOffset(DateTime.UtcNow),
+                Generator = "Google Research Web2Feed",
                 ImageUrl = new Uri("https://research.google.com/favicon.ico"),
             };
 
@@ -146,7 +148,7 @@ namespace GoogleResearchWeb2Feed
             foreach (var article in articles)
             {
                 // TODO: Fill the item content
-                var htmlContent = article.Abstract;
+                var htmlContent = article.Abstract ?? @"T/O or PDF";
 
                 var item = new SyndicationItem
                 {
@@ -176,6 +178,7 @@ namespace GoogleResearchWeb2Feed
             public string Title { get; set; }
             public string Abstract { get; set; }
             public Uri Uri { get; set; }
+            public Uri AbstractUri { get; set; }
             public Uri SearchUri { get; set; }
             public Uri PdfUri { get; set; }
             public string Appendix { get; set; }
--- Src/HostWeb/Controllers/FeedController.cs
+++ Src/HostWeb/Controllers/FeedController.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.UI;
 using System.Xml;
 
 namespace GoogleResearchWeb2Feed.Controllers
@@ -15,14 +17,19 @@ namespace GoogleResearchWeb2Feed.Controllers
     public class FeedController : Controller
     {
         // GET /Feed/Rss
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Server)]
         public async Task<ActionResult> Rss()
         {
             var grWeb2Feed = new GoogleResearchWeb2Feed();
 
             using (var sw = new StringWriter())
             {
-                var xml = new XmlTextWriter(sw);
-                (await grWeb2Feed.GetFeed()).SaveAsRss20(xml);
+                var formatter = new Rss20FeedFormatter(await grWeb2Feed.GetFeed());
+
+                using (var xml = new XmlTextWriter(sw))
+                {
+                    formatter.WriteTo(xml);
+                }
                 return Content(sw.ToString(), "application/rss+xml", Encoding.UTF8);
             }
         }
